a9c4cbb2790a55db35d32d6d012e019c7ce67883
rajpagare01 <rajpagare305@gmail.com>
2026-03-05 20:56:12 +0530

    Crud operation

---
+++ 2CRUDREST/src/main/java/com/CRUDREST/Controller/UserController.java
@@ -0,0 +1,48 @@
+package com.CRUDREST.Controller;
+
+import com.CRUDREST.Model.User;
+import com.CRUDREST.Service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
+public class UserController {
+
+    private final UserService userService;
+    UserController(UserService userService) {
+    this.userService = userService;
+    }
+
+
+    @PostMapping("/users")
+    public ResponseEntity<User>createUser( @RequestBody User user)
+    {
+        return userService.createUser(user);
+    }
+
+
+    @GetMapping("/users")
+    public ResponseEntity<?> getAllUsers()
+    {
+        return userService.getAllUsers();
+    }
+
+    @PutMapping("/users/{id}")
+    public Optional<ResponseEntity<User>> updateUser(@PathVariable int id, @RequestBody User user)
+    {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public ResponseEntity<?> deleteUser(@PathVariable int id)
+    {
+        return userService.deleteUser(id);
+    }
+
+
+
+
+}
---
+++ 2CRUDREST/src/main/java/com/CRUDREST/Repository/UserRepo.java
@@ -0,0 +1,8 @@
+package com.CRUDREST.Repository;
+
+import com.CRUDREST.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Integer> {
+
+}
